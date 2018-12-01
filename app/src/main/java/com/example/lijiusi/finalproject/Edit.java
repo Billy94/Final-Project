bda8b05e1fa0301ba99c0f9b40cc96881f281b96
lijiusi <jiusili2@illinois.edu>
2018-12-01 14:56:50 -0600

    .

---
+++ app/src/main/java/com/example/lijiusi/finalproject/Edit.java
@@ -0,0 +1,5 @@
+package com.example.lijiusi.finalproject;
+
+public class Edit {
+
+}
--- app/src/main/java/com/example/lijiusi/finalproject/MainActivity.java
+++ app/src/main/java/com/example/lijiusi/finalproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lijiusi.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView test = findViewById(R.id.test);
+        Button semester1 = findViewById(R.id.semester1);
+        semester1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
