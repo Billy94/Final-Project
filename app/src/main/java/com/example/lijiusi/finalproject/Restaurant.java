5a3140fe9530bcea7aa65514fe0c01c76cf2ac7c
lijiusi <jiusili2@illinois.edu>
2018-12-01 15:51:36 -0600

    .

--- app/src/main/java/com/example/lijiusi/finalproject/Restaurant.java
+++ app/src/main/java/com/example/lijiusi/finalproject/Restaurant.java
@@ -3,7 +3,7 @@ package com.example.lijiusi.finalproject;
 public class Restaurant {
     private int price;
     private String name;
-    public Restaurant(final int setPrice, final String setName) {
+    public Restaurant(final String setName, final int setPrice) {
         price = setPrice;
         name = setName;
     }
--- app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
+++ app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
@@ -3,5 +3,5 @@ package com.example.lijiusi.finalproject;
 public class RestaurantArr {
     private Restaurant[] restaurantPool;
     private Restaurant[] copyPool;
-
+    Restaurant r1 = new Restaurant("Hot Pot Lab", 3);
 }
