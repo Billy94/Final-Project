cc664df3dec221746bc4dd767a9563327b742cf6
davidlin <davidlinyiyang@gmail.com>
2018-12-01 15:59:29 -0600

    1

--- app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
+++ app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
@@ -4,4 +4,24 @@ public class RestaurantArr {
     private Restaurant[] restaurantPool;
     private Restaurant[] copyPool;
     Restaurant r1 = new Restaurant("Hot Pot Lab", 3);
+    Restaurant r2 = new Restaurant("Shiquan", 2);
+    Restaurant r3 = new Restaurant("Lai Lai Wok", 1);
+    Restaurant r4 = new Restaurant("BoBo China", 1);
+    Restaurant r5 = new Restaurant("Mr Chou and Charles", 2);
+    Restaurant r6 = new Restaurant("Nanjing Bistro", 1);
+    Restaurant r7 = new Restaurant("Taipei Cafe", 1);
+    Restaurant r8 = new Restaurant("Spicy Tang", 2);
+    Restaurant r9 = new Restaurant("Carvings", 1);
+    Restaurant r10 = new Restaurant("Mandarin Wok", 2);
+    Restaurant r11 = new Restaurant("Mid Summer Lounge", 1);
+    Restaurant r12 = new Restaurant("New Asian Taste", 1);
+    Restaurant r13 = new Restaurant("Kung Fu BBQ", 1);
+    Restaurant r14 = new Restaurant("", 1);
+    Restaurant r6 = new Restaurant("", 1);
+    Restaurant r6 = new Restaurant("", 1);
+    Restaurant r6 = new Restaurant("", 1);
+    Restaurant r6 = new Restaurant("", 1);
+
+
+
 }
