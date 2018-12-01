d8727911aecb4e791b11a1b65f784f876328ff97
davidlin <davidlinyiyang@gmail.com>
2018-12-01 16:08:53 -0600

    1

--- app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
+++ app/src/main/java/com/example/lijiusi/finalproject/RestaurantArr.java
@@ -1,8 +1,6 @@
 package com.example.lijiusi.finalproject;
 
 public class RestaurantArr {
-    private Restaurant[] restaurantPool = {r1};
-    private Restaurant[] copyPool;
     Restaurant r1 = new Restaurant("Hot Pot Lab", 3);
     Restaurant r2 = new Restaurant("Shiquan", 2);
     Restaurant r3 = new Restaurant("Lai Lai Wok", 1);
@@ -16,12 +14,15 @@ public class RestaurantArr {
     Restaurant r11 = new Restaurant("Mid Summer Lounge", 1);
     Restaurant r12 = new Restaurant("New Asian Taste", 1);
     Restaurant r13 = new Restaurant("Kung Fu BBQ", 1);
-    Restaurant r14 = new Restaurant("", 1);
-    Restaurant r6 = new Restaurant("", 1);
-    Restaurant r6 = new Restaurant("", 1);
-    Restaurant r6 = new Restaurant("", 1);
-    Restaurant r6 = new Restaurant("", 1);
-
-
+    Restaurant r14 = new Restaurant("Evo Cafe", 1);
+    Restaurant r15 = new Restaurant("Home of Gourmet Chinese and Thai", 1);
+    Restaurant r16 = new Restaurant("Lao Sze Chuan", 2);
+    Restaurant r17 = new Restaurant("Golden Harbor Authentic Chinese Cuisine", 2);
+    Restaurant r18 = new Restaurant("Rainbow Garden", 2);
+    Restaurant r19 = new Restaurant("South China", 1);
+    Restaurant r20 = new Restaurant("Panda Express", 1);
+    Restaurant r21 = new Restaurant("Hot Wok Express", 1);
+    private Restaurant[] restaurantPool = {r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21};
+    private Restaurant[] copyPool;
 
 }
