b57da111a8fad98dd951af9dd617c1abbb753798
davidlin <davidlinyiyang@gmail.com>
2018-12-04 10:18:41 -0600

    1

--- app/src/main/java/com/example/lijiusi/finalproject/MainActivity.java
+++ app/src/main/java/com/example/lijiusi/finalproject/MainActivity.java
@@ -3,21 +3,34 @@ package com.example.lijiusi.finalproject;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.MapView;
 import com.yelp.fusion.client.connection.YelpFusionApi;
 import com.yelp.fusion.client.connection.YelpFusionApiFactory;
 import com.yelp.fusion.client.models.Business;
 
+
+
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static com.android.volley.Request.*;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "final project";
@@ -33,11 +46,13 @@ public class MainActivity extends AppCompatActivity {
         randomBotton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //yelpAPICall();
-                restaurantTextView.setText(RestaurantArr.generateRandom().getName());
+
+                startAPICall();
+                //restaurantTextView.setText(RestaurantArr.generateRandom().getName());
             }
         });
     }
+    /*
     void yelpAPICall() {
         try {
             YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
@@ -81,6 +96,47 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean isOnDestroyCalled() {
         return this.isOnDestroyCalled;
+    }
+
+    */
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw",
+                    null,
+                    new com.android.volley.Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                            processResponse(response);
+                        }
+                    }, new com.android.volley.Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            }) {
+                public Map<String, String> getHeaders() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Bearer", "XY3qKK8bXCSqfj5Fgzzac1JHIpTkGXySsYI5TIJyefWseq6lBhEyq7v0PFH9gTfpNHmSrLzxYtjFZjFVex4xx7ShYpLc3TZ6rSy_SNaoUngTs2Af0y8BKjfkyI0FXHYx");
+                    Log.d(TAG, params.toString());
+                    return params;
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void processResponse(final JSONObject response) {
+        try {
+            TextView restaurantTextView = findViewById(R.id.restaurantTextView);
+            restaurantTextView.setText(response.getString("name"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 /*
